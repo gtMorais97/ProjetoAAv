b0746a517787e33b4e6d9a4c21d556ca2107dfd6
reandreev <reandreev@outlook.com>
2020-05-27 23:55:18 +0100

    Change colors of graph

--- cc_tests.py
+++ cc_tests.py
@@ -7,7 +7,7 @@ from clustering_coefficient import uniform_wedge, uniform_edge, uniform_vertex
 cwd = os.getcwd()
 graph = csr.CSR(tsv_file=f"{cwd}\\tsv_graphs\\out.com-amazon")
 
-iterations = 10
+iterations = 5
 triangle_count = 667129
 sample_size = 300
 
@@ -22,12 +22,12 @@ for i in range(iterations):
     edge = np.append(edge, uniform_edge(graph, sample_size))
     vertex = np.append(vertex, uniform_vertex(graph, sample_size))
 
-plt.plot(x, wedge, '-o', color='b')
-plt.plot(x, edge, '-o', color='r')
-plt.plot(x, vertex, '-o', color='g')
-plt.axhline(y=np.mean(wedge), color='b', linestyle='--')
-plt.axhline(y=np.mean(edge), color='r', linestyle='--')
-plt.axhline(y=np.mean(vertex), color='g', linestyle='--')
+plt.plot(x, wedge, '-o', color='tab:blue')
+plt.plot(x, edge, '-o', color='tab:red')
+plt.plot(x, vertex, '-o', color='tab:green')
+plt.axhline(y=np.mean(wedge), color='tab:blue', linestyle='--')
+plt.axhline(y=np.mean(edge), color='tab:red', linestyle='--')
+plt.axhline(y=np.mean(vertex), color='tab:green', linestyle='--')
 plt.axhline(y=triangle_count, color='k', linestyle='--')
 plt.legend(["Uniform wedge", "Uniform edge", "Uniform vertex",
             "Uniform wedge mean", "Uniform edge mean", "Uniform vertex mean", "Real"])
