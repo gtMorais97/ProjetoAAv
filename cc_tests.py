fd9f1fc122c61a99addad2031f34fc2a5c258be7
reandreev <reandreev@outlook.com>
2020-05-28 11:47:00 +0100

    Add labels

--- cc_tests.py
+++ cc_tests.py
@@ -7,9 +7,9 @@ from clustering_coefficient import uniform_wedge, uniform_edge, uniform_vertex
 cwd = os.getcwd()
 graph = csr.CSR(tsv_file=f"{cwd}\\tsv_graphs\\out.com-amazon")
 
-iterations = 5
+iterations = 10
 triangle_count = 667129
-sample_size = 300
+sample_size = 500
 
 x = np.array([*range(1, iterations + 1)])
 wedge = np.array([])
@@ -31,4 +31,7 @@ plt.axhline(y=np.mean(vertex), color='tab:green', linestyle='--')
 plt.axhline(y=triangle_count, color='k', linestyle='--')
 plt.legend(["Uniform wedge", "Uniform edge", "Uniform vertex",
             "Uniform wedge mean", "Uniform edge mean", "Uniform vertex mean", "Real"])
+plt.title(f"Approximate triangle counts with a sample size of {sample_size}")
+plt.ylabel("Triangle count")
+plt.xlabel("Iteration")
 plt.show()
